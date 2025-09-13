bda59c1b63660aae10d8895990d75f39589dec79
William Chan <william@chan888.com>
2025-09-13 20:16:10 +1200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 # libgl1-mesa-glx need to fix libGL.so.1 issue
 # https://github.com/PaddlePaddle/PaddleSeg/issues/3435
-apt install libgl1-mesa-glx
+apt update -y
+apt install libgl1-mesa-glx -y
 
 pip install -r requirements.txt
